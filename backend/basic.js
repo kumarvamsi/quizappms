0d1883dc03891de728d9ec7bd79343683175f92b
connect vamsi <kumarbvamsi@gmail.com>
2019-02-15 22:56:17 +0530

    added some changes

---
+++ backend/basic.js
@@ -0,0 +1,38 @@
+
+let i=12;
+let s="Hello";
+let obj={
+    "name":"Ramu",
+    "age":23
+};
+
+let arr=[{
+    "name":"Ramu",
+    "age":23
+},{
+    "name":"Ramu",
+    "age":23
+},{
+    "name":"Ramu",
+    "age":23
+}]
+
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i].name);
+}
+
+
+if(i>23){
+    console.log("i is more");
+
+}
+else{
+    console.log("i is less");
+}
+
+    //Number
+//String
+//undefined
+//object
+//null
+//array
---
+++ backend/index.js
@@ -0,0 +1,20 @@
+let sayHello =function(name="aunknown"){
+
+    console.log("say hello to "+name);
+
+}
+
+let arrow=()=>{console.log("arrow")};
+
+function sayHi(){
+    console.log("Hi");
+
+}
+
+
+arrow();
+sayHi();
+
+sayHello();
+
+sayHello("klj");
